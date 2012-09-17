965c8e3266e8b312f9468d3b49dc2d55675442ff
witek <witekdev@gmail.com>
2012-09-17 12:08:39 +0200

    modify edit form buttons, created basic static topic tree with checkboxes.

--- collective/topictree/static/js/edittopictree.js
+++ collective/topictree/static/js/edittopictree.js
@@ -6,23 +6,28 @@ jq("#treeroot")
 	.jstree({ 
 		// List of active plugins
 		"plugins" : [ 
-			"themes","ui","crrm","dnd","search","types","contextmenu","hotkeys","cookies"//,"json_data",
+			"themes","ui","crrm","contextmenu","checkbox","html_data"//,"json_data",
 		],
-		// UI & core - the nodes to initially select and open will be overwritten by the cookie plugin
-
-		// the UI plugin - it handles selecting/deselecting/hovering nodes
-		"ui" : {
-			// this makes the node with ID node_4 selected onload
-//			"initially_select" : [ "node_4" ]
-		},
 		// the core plugin - not many options here
 		"core" : { 
 			// just open those two nodes up
 			// as this is an AJAX enabled tree, both will be downloaded from the server
-//			"initially_open" : [ "node_2" , "node_3" ] 
-		}
+			"initially_open" : [ "root2"  ] 
+		},
+        "html_data" : {
+            "data" : "<ul><li id='root'><a href='#'>Root node</a>\
+                      <ul><li><a href='#'>Child node</a></li><li><a href='#'>Child node2</a></li></ul></li>\
+                      <li id='root2'><a href='#'>Another root node</a>\
+                      <ul><li><a href='#'>Child node</a></li></ul></li></ul>"
+
+        },
+        "checkbox" : {
+            "two_state" : true
+           // "override_ui" : true
+        }
 	});
 
+jq("#treeroot").jstree("set_theme","classic"); //doesnt work yet
 
     console.log("post tree debug message");
 
