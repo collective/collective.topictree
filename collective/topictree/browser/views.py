c072a551d672cf1018a64a9a8fc49731a1c75725
witek <witekdev@gmail.com>
2012-09-24 16:28:42 +0200

    add EditTopicView and DeleteTopicView on the server side, AddTopicView now correctly saves the object title on the server side. rename and delete buttons and not working 100% yet, code clean up.

--- collective/topictree/browser/views.py
+++ collective/topictree/browser/views.py
@@ -3,12 +3,16 @@ import json
 from five import grok
 
 from zope.interface import Interface
+from zope.event import notify
+from zope.lifecycleevent import ObjectModifiedEvent
 
 from collective.topictree import MessageFactory as _
 from collective.topictree.topictree import ITopicTree
 
 from plone.uuid.interfaces import IUUID
 from plone.dexterity.utils import createContentInContainer
+
+from Products.CMFCore.utils import getToolByName
 
 grok.templatedir('templates')
 
@@ -22,16 +26,10 @@ class AddTopicView(grok.View):
     def __call__(self):
         request = self.request
         context = self.context
-        topic_title = request.get('topic_title', '')
-        if not topic_title:
-            return
-
         topic = createContentInContainer(context, "collective.topictree.topic",
-                                                             title=topic_title) 
-
+                                                             title='New Topic') 
         result = 'success'
         return json.dumps({ 'result'   : result,
-                            'title'    : topic_title,
                             'node_uid' : IUUID(topic),
                             'path'     : topic.absolute_url()})
 
@@ -40,3 +38,70 @@ class AddTopicView(grok.View):
         """
         return ''
 
+
+class EditTopicView(grok.View):
+    """ Edit Topic on the topic tree
+    """
+    grok.context(ITopicTree)
+    grok.name('edittopic') 
+    grok.require('zope2.View')
+
+    def __call__(self):
+        request = self.request
+        context = self.context
+        topic_title = request.get('topic_title', '')
+        node_uid = request.get('node_uid', '')
+        if not topic_title and not node_uid:
+            return
+   
+        # find the node object
+        catalog = getToolByName(context, 'portal_catalog')
+        brains = catalog(
+                 portal_type='collective.topictree.topic',
+                 UID=node_uid)
+        obj = brains[0].getObject()
+
+        # set the new title
+        obj.title = topic_title
+        notify(ObjectModifiedEvent(obj))
+
+        result = 'success'
+        return json.dumps({ 'result' : result})
+
+    def render(self):
+        """ No-op to keep grok.View happy
+        """
+        return ''
+
+class DeleteTopicView(grok.View):
+    """ Delete Topic from the topic tree
+    """
+    grok.context(ITopicTree)
+    grok.name('deletetopic') 
+    grok.require('zope2.View')
+
+    def __call__(self):
+        request = self.request
+        context = self.context
+        node_uid = request.get('node_uid', '')
+        if not node_uid:
+            return
+    
+        # find the node object
+        catalog = getToolByName(context, 'portal_catalog')
+        brains = catalog(
+                 portal_type='collective.topictree.topic',
+                 UID=node_uid)
+        obj = brains[0].getObject()
+
+        # delete the object
+        parent = obj.aq_parent
+        parent.manage_delObjects([obj.getId()])
+
+        result = 'success'
+        return json.dumps({ 'result' : result})
+
+    def render(self):
+        """ No-op to keep grok.View happy
+        """
+        return ''
--- collective/topictree/static/js/edittopictree.js
+++ collective/topictree/static/js/edittopictree.js
@@ -1,46 +1,50 @@
 $(document).ready(function() {
 
 
-jq(function () {
-        jq("#mmenu input").click(function () {
+$(function () {
+        $("#mmenu input").click(function () {
             switch(this.id) {
                 case "add_topic":
-                    jq.ajax({
+                    $.ajax({
                             url: "@@addtopic",
-                            data: {
-                                    'topic_title': 'test'
-                            },
+//                            data: {
+//                                    'topic_title': 'test'
+//                            },
                             success: createNode,
                             error: displayError,
                             dataType: "json",
                             context: this
                     });
+                    break;
+                case "rename":
+                    $("#treeroot").jstree(this.id);
 
-                    break;
-                default:
-                    jq("#treeroot").jstree(this.id);
-                    break;
+                case "remove":
+                    $("#treeroot").jstree(this.id);
+
+//                default:
+//                    $("#treeroot").jstree(this.id);
+//                    break;
             }
         });
 });
 
-
-jq("#treeroot")
+$("#treeroot")
 	.jstree({ 
 		// List of active plugins
 		"plugins" : [ 
-			"themes","ui","crrm","contextmenu","checkbox","html_data"//,"json_data",
+			"themes","ui","crrm","contextmenu","checkbox","html_data"
 		],
 		"core" : { 
 //			"initially_open" : [ "root2"  ] 
 		},
-        "html_data" : {
-            "data" : "<li id='root'><a href='#'>Root node</a></li>"
+//        "html_data" : {
+//            "data" : "<li id='root'><a href='#'>Root node</a></li>"
 //            "data" : "<ul><li id='root'><a href='#'>Root node</a>\
 //                      <ul><li><a href='#'>Child node</a></li><li><a href='#'>Child node2</a></li></ul></li>\
 //                      <li id='root2'><a href='#'>Another root node</a>\
 //                      <ul><li><a href='#'>Child node</a></li></ul></li></ul>"
-        },
+//        },
         "checkbox" : {
             "two_state" : true
         },
@@ -49,31 +53,64 @@ jq("#treeroot")
                 "dots" : false,
                 "icons" : false
         }
-	});
+    })
+    //this fires after an item is edited in the tree
+    .bind("rename.jstree", function (e, data) {
+       console.log("renamed");
+
+    })
+    //this fires after an item is removed from the tree
+    .bind("remove.jstree", function (e, data) {
+       console.log("and its gone..");
+
+    });
 
 });
 
 function createNode(data, textStatus, jqXHR) {
 
-    var title = data.title;
     var node_uid = data.node_uid;
     var path = data.path;
 
-    jq("#treeroot").jstree("create",
+    $("#treeroot").bind("create.jstree", function (e, data) {
+                       console.log("created a new node");
+                       var topic_title = $('[node_uid="' + node_uid + '"] > a')
+                                          .text().trim();
+                       console.log(topic_title);
+                       $.ajax({
+                               url: "@@edittopic",
+                               data: {
+                                      'topic_title': topic_title,
+                                      'node_uid': node_uid
+                               },
+                               //success: createNode,
+                               //error: displayError,
+                               dataType: "json",
+                               context: this
+                       });
+                       // remove binding so this is called only once
+                       // (not everytime another object is created)
+                       $(this).unbind("create.jstree");
+
+                  })
+                  .jstree("create",
                            null,
                            "last", 
                            { "attr" : { "rel" : 
                                         this.id.toString().replace("add_", ""),
                                         "node_uid" : node_uid,
                                         "path" : path
-                                      }
+                                      },
+                           "data" : "New node" // specify default new text
                            });
-    li a 
+
+}
+
+function renameNode(data, textStatus, jqXHR) {
+
 }
 
 function displayError(jqXHR, textStatus, errorThrown) {
     alert(errorThrown);
 }
-
-
 
